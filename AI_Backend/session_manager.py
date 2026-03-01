8529d5f13c6782d8d4386f7e4eb4f406016135fa
GaurviiiPrajapati <gaurviprajapati23@gmail.com>
2026-03-01 14:26:41 +0530

    ..

--- AI_Backend/session_manager.py
+++ AI_Backend/session_manager.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 from typing import Dict, Optional, List
 import uuid
-from .conversation import Conversation, UserSession, Message
+from conversation import Conversation, UserSession, Message
 
 
 class SessionManager:
